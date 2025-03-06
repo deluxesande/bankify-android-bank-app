9be2e358974fd28c6d679bab7d138c1445702163
deluxesande <deluxesande@gmail.com>
2025-03-06 16:02:11 +0300

    Added card view

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,4 +41,5 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.cardview:cardview:1.0.0")
 }
